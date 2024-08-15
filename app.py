a1750998c62844628e99af5ae5bd4cfe31f877ca
DurgaJasvitha <jasvitha.bommisetty1@gmail.com>
2024-08-15 16:09:30 +0530

    idk

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 def addition(a,b):
     return a+b
 
-def subtraction(a,b):
-    return a-b
+def subtraction(a,b,c):
+    return a-b-c
 
-def division(a,b):
-    return a/b
+def division(a,b,c):
+    return a/b*c
 
 def mul(a,b):
     return a*b
